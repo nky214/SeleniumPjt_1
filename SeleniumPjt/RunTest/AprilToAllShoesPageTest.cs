12a4cf9a0269c74be83e6b943b675002f91d71ac
nky214 <nky214@gmail.com>
2022-02-11 06:25:03 -0500

    April Test Case

---
+++ SeleniumPjt/RunTest/AprilToAllShoesPageTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumPjt.RunTest
+{
+    internal class AprilToAllShoesPageTest : RunTest
+    {
+        //April to AllShoes Page has same structure with MarchPageTest, so we can reuse MarchPageTest.
+    }
+}
